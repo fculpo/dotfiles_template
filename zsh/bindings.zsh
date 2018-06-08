2f20e8ee5217d6822e455cabae6d6e56526b6909
Fabien Culpo <fabien.culpo@gmail.com>
2018-06-08 19:28:41 +0200

    add bindings for history substring

--- zsh/bindings.zsh
+++ zsh/bindings.zsh
@@ -7,3 +7,9 @@ bindkey "^g" end-of-line # TODO: change to ctrl + : instead
 bindkey "^k" forward-word
 bindkey "^j" backward-word
 bindkey "^e" kill-whole-line
+
+# History substring search
+bindkey '^[OA' history-substring-search-up
+bindkey '^[OB' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
