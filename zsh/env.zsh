97c4f5d70c3c194508a47f6bd9600a820f3974ca
Fabien Culpo <fabien.culpo@gmail.com>
2018-06-15 22:58:46 +0200

    add local bins to path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -13,9 +13,10 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export NODE_PATH='/usr/local/lib/node_modules'
 
 # PATH
-PATH=$PATH:/usr/local/bin
-PATH=$PATH:$HOME/.dotfiles/bin
-PATH=$PATH:$HOME/.rbenv/bin
+PATH=/usr/local/bin:$PATH
+PATH=$HOME/.dotfiles/bin:$PATH
+PATH=$HOME/.rbenv/bin:$PATH
+PATH=$HOME/.local/bin:$PATH
 
 # Remove Windows paths to Ruby since it causes conflicts with npm/bower/etc.
 PATH=$(echo :$PATH: | sed -e 's,:/mnt/c/.*:,:,g' -e 's/^://' -e 's/:$//')
