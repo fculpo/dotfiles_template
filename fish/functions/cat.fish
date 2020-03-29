5a5443adaa0f8c118daa59cbe3c41315ce6f0455
Fabien Culpo <fabien.culpo@gmail.com>
2020-03-29 18:27:20 +0200

    fix cat and rbenv

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,14 +1,8 @@
-function cat -d "Use bat instead of cat unless it's a Markdown file, then use mdless"
-    set -l exts md markdown txt
-
+function cat -d "Use bat instead of cat"
     if not test -f $argv
         echo "File not found: $argv"
         return 0
     end
 
-    if contains (get_ext $argv) $exts
-        mdless $argv
-    else
-        command bat --style plain $argv
-    end
+    command bat --style plain $argv
 end
